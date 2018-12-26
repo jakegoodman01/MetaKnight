831cd100d15f10506803e17fca4910645db6cbbd
Jake Goodman <jakewgoodman@gmail.com>
2018-12-26 13:45:01 -0400

    Wrote tests for Square.up

--- tests/test_board.py
+++ tests/test_board.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from metaknight import Board, Square
+from metaknight import Board, Square, OutOfBoundsError
 
 
 class SquareTests(TestCase):
@@ -10,6 +10,16 @@ class SquareTests(TestCase):
         self.assertEqual(self.square.rank, '1')
         self.assertEqual(self.square.file, 'a')
 
+    def test_up(self):
+        self.assertEqual(self.square.up(), Square('b1'))
+        self.assertEqual(Square('b1').up(), Square('c1'))
+        self.assertEqual(Square('c1').up(), Square('d1'))
+        self.assertEqual(Square('d1').up(), Square('e1'))
+        self.assertEqual(Square('e1').up(), Square('f1'))
+        self.assertEqual(Square('f1').up(), Square('g1'))
+        self.assertEqual(Square('g1').up(), Square('h1'))
+        self.assertRaises(lambda: OutOfBoundsError, Square('h1').up())
+
 
 class BoardTests(TestCase):
     def setUp(self):
@@ -17,4 +27,6 @@ class BoardTests(TestCase):
 
     def test_board_construction(self):
         self.assertEqual(len(self.board.squares), 8)
-        self.assertEqual(len(self.board.squares)[0], 8)
+        self.assertEqual(len(self.board.squares[0]), 8)
+        self.assertEqual(self.board.squares[0][0], Square('a1'))
+        self.assertEqual(self.board.squares[7][7], Square('h8'))
