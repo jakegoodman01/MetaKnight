caf3bd8e06f860a921fd77d5bc4aade40a94b4d5
Jake Goodman <jakewgoodman@gmail.com>
2018-12-28 15:52:49 -0400

    Changed get_square_at_location to accept either a string or a Square. And modified all of its uses to accomidate this change

--- metaknight/board.py
+++ metaknight/board.py
@@ -133,100 +133,107 @@ class Board:
         self.squares[7][3].piece = Piece(PieceType.QUEEN, Color.BLACK)
         self.squares[7][4].piece = Piece(PieceType.KING, Color.BLACK)
 
-    def get_square_at_location(self, location: str) -> Square:
+    def get_square_at_location(self, location=None, square=None) -> Square:
         """
         :param location: string coordinates of a square. For example: 'a3' or 'c8'
+        :param square: a square object that is not located in this board
         :return: the square at location in this board
         """
-        file = Board.files.index(location[0])
-        rank = Board.ranks.index(location[1])
+        if location:
+            file = Board.files.index(location[0])
+            rank = Board.ranks.index(location[1])
+        elif square:
+            file = Board.files.index(square.file)
+            rank = Board.ranks.index(square.rank)
+        else:
+            raise ValueError('Must enter either a string location, or a Square object')
         return self.squares[rank][file]
 
     def pawn_moves(self, square: Square) -> List[List[Square]]:
         # TODO: Account for en passant
         moves = []
-        square = self.get_square_at_location(repr(square))
+        square = self.get_square_at_location(square=square)
         color = square.piece.color
         forward = Square.up
         rank = '2'
         if square.piece.color is Color.BLACK:
             forward = Square.down
             rank = '7'
-            
-        forward_square = self.get_square_at_location(repr(forward(square)))
+
+        forward_square = self.get_square_at_location(square=forward(square))
         if forward_square.piece is None:
             moves.append([forward_square])
-            forward_square = self.get_square_at_location(repr(forward(forward_square)))
+            forward_square = self.get_square_at_location(square=forward(forward_square))
             if square.rank == rank and forward_square.piece is None:
                 moves[0].append(forward_square)
         if square.file != 'a':
-            diagonal = self.get_square_at_location(repr(forward(square).left()))
+            diagonal = self.get_square_at_location(square=forward(square).left())
             if diagonal.piece and diagonal.piece.color is not color:
                 moves.append([diagonal])
         if square.file != 'h':
-            diagonal = self.get_square_at_location(repr(forward(square).right()))
+            diagonal = self.get_square_at_location(square=forward(square).right())
             if diagonal.piece and diagonal.piece.color is not color:
                 moves.append([diagonal])
         return moves
 
     def knight_moves(self, square: Square) -> List[List[Square]]:
         # TODO: Make this less horrible :)
-        original = self.get_square_at_location(repr(square))
+        original = self.get_square_at_location(square=square)
         color = original.piece.color
         moves = []
 
         # upper left
         try:
-            square = self.get_square_at_location(repr(original.up().up().left()))
+            square = self.get_square_at_location(square=original.up().up().left())
             if not square.piece or square.piece.color is not color:
                 moves.append([square])
         except OutOfBoundsError:
             pass
         # upper right
         try:
-            square = self.get_square_at_location(repr(original.up().up().right()))
+            square = self.get_square_at_location(square=original.up().up().right())
             if not square.piece or square.piece.color is not color:
                 moves.append([square])
         except OutOfBoundsError:
             pass
         # lower left
         try:
-            square = self.get_square_at_location(repr(original.down().down().left()))
+            square = self.get_square_at_location(square=original.down().down().left())
             if not square.piece or square.piece.color is not color:
                 moves.append([square])
         except OutOfBoundsError:
             pass
         # lower right
         try:
-            square = self.get_square_at_location(repr(original.down().down().right()))
+            square = self.get_square_at_location(square=original.down().down().right())
             if not square.piece or square.piece.color is not color:
                 moves.append([square])
         except OutOfBoundsError:
             pass
         # left upper
         try:
-            square = self.get_square_at_location(repr(original.left().left().up()))
+            square = self.get_square_at_location(square=original.left().left().up())
             if not square.piece or square.piece.color is not color:
                 moves.append([square])
         except OutOfBoundsError:
             pass
         # left lower
         try:
-            square = self.get_square_at_location(repr(original.left().left().down()))
+            square = self.get_square_at_location(square=original.left().left().down())
             if not square.piece or square.piece.color is not color:
                 moves.append([square])
         except OutOfBoundsError:
             pass
         # right upper
         try:
-            square = self.get_square_at_location(repr(original.right().right().up()))
+            square = self.get_square_at_location(square=original.right().right().up())
             if not square.piece or square.piece.color is not color:
                 moves.append([square])
         except OutOfBoundsError:
             pass
         # right lower
         try:
-            square = self.get_square_at_location(repr(original.right().right().down()))
+            square = self.get_square_at_location(square=original.right().right().down())
             if not square.piece or square.piece.color is not color:
                 moves.append([square])
         except OutOfBoundsError:
@@ -236,15 +243,15 @@ class Board:
     def bishop_moves(self, square: Square) -> List[List[Square]]:
         # TODO: Make this less horrible :)
         moves = []
-        original = self.get_square_at_location(repr(square))
+        original = self.get_square_at_location(square=square)
         color = original.piece.color
         diagonal = []
         try:
             # upper left diagonal
             square = original
             while True:
-                square = self.get_square_at_location(repr(square.up()))
-                square = self.get_square_at_location(repr(square.left()))
+                square = self.get_square_at_location(square=square.up())
+                square = self.get_square_at_location(square=square.left())
                 if not square.piece:
                     diagonal.append(square)
                 elif square.piece.color is not color:
@@ -259,8 +266,8 @@ class Board:
             # upper right diagonal
             square = original
             while True:
-                square = self.get_square_at_location(repr(square.up()))
-                square = self.get_square_at_location(repr(square.right()))
+                square = self.get_square_at_location(square=square.up())
+                square = self.get_square_at_location(square=square.right())
                 if not square.piece:
                     diagonal.append(square)
                 elif square.piece.color is not color:
@@ -275,8 +282,8 @@ class Board:
             # lower left diagonal
             square = original
             while True:
-                square = self.get_square_at_location(repr(square.down()))
-                square = self.get_square_at_location(repr(square.left()))
+                square = self.get_square_at_location(square=square.down())
+                square = self.get_square_at_location(square=square.left())
                 if not square.piece:
                     diagonal.append(square)
                 elif square.piece.color is not color:
@@ -291,8 +298,8 @@ class Board:
             # lower right diagonal
             square = original
             while True:
-                square = self.get_square_at_location(repr(square.down()))
-                square = self.get_square_at_location(repr(square.right()))
+                square = self.get_square_at_location(square=square.down())
+                square = self.get_square_at_location(square=square.right())
                 if not square.piece:
                     diagonal.append(square)
                 elif square.piece.color is not color:
