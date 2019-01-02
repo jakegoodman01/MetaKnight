9aab9cd2febbbec6e8b2c87f41e26325abe08106
Jake Goodman <jakewgoodman@gmail.com>
2019-01-02 18:59:43 -0500

    Wrote en passant tests

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from metaknight.game import Game
+from metaknight.move import InvalidNotationError
 from metaknight.piece import Piece, PieceType, Color
 
 
@@ -17,3 +18,19 @@ class TestGame(TestCase):
         self.assertEqual(self.game.board.get_square(location='b8').piece, None)
         self.assertEqual(self.game.board.get_square(location='c6').piece, Piece(PieceType.KNIGHT, Color.BLACK))
         self.assertEqual(self.game.to_move, Color.WHITE)
+
+    def test_en_passant(self):
+        self.game.play_move('e4')
+        self.game.play_move('f5')
+        self.game.play_move('exf5')
+        self.game.play_move('g5')
+        self.game.play_move('fxg6')
+        self.game.play_move('hxg6')
+        self.game.play_move('h4')
+        self.game.play_move('g5')
+        self.game.play_move('a3')
+        self.game.play_move('g4')
+        self.game.play_move('f4')
+        self.assertRaises(InvalidNotationError, lambda: self.game.play_move('gxh3'))
+        self.game.play_move('gxf3')
+
