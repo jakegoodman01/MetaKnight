905ddbb0402f7c249aca2b2ea242a05be6cb0bdc
Jake Goodman <jakewgoodman@gmail.com>
2019-01-08 08:39:01 -0500

    Completed Castle and completed en passant

--- metaknight/move.py
+++ metaknight/move.py
@@ -9,10 +9,6 @@ class InvalidNotationError(Exception):
     pass
 
 
-class CannotCastleError(Exception):
-    pass
-
-
 class Move:
     def __init__(self, board: Board, to_move: Color, origin: Square, destination: Square, en_passant: bool=False):
         """
@@ -27,18 +23,25 @@ class Move:
         self.origin: Square = board.get_square(square=origin)
         self.destination: Square = board.get_square(square=destination)
         self.piece_moved: Piece = self.origin.piece
+        self.en_passant = en_passant
 
-        if en_passant:
+        if self.en_passant:
             func = Square.up if self.to_move is Color.BLACK else Square.down
             square = func(self.destination)
             self.piece_captured: Piece = self.board.get_square(square=square).piece
         else:
+            if self.destination.piece and self.destination.piece.color == to_move:
+                raise InvalidNotationError
             self.piece_captured: Piece = self.destination.piece  # None if no piece was captured
 
     def execute_move(self):
         self._not_in_check()
         self.board.get_square(square=self.origin).piece = None
         self.board.get_square(square=self.destination).piece = self.piece_moved
+        if self.en_passant:
+            func = Square.up if self.to_move is Color.BLACK else Square.down
+            square = func(self.destination)
+            self.board.get_square(square=square).piece = None
 
         self.origin: Square = self.board.get_square(square=self.origin)
         self.destination: Square = self.board.get_square(square=self.destination)
@@ -206,9 +209,10 @@ class Move:
 
 
 class Castle:
-    def __init__(self, board: Board, to_move: Color, king_side: bool=True):
-        if board.in_check(to_move):
-            raise CannotCastleError
+    def __init__(self, board: Board, to_move: Color,
+                 king_side: bool=True, king_moved: bool=False, rook_moved: bool=False):
+        if board.in_check(to_move) or king_moved or rook_moved:
+            raise InvalidNotationError
         rank = '1' if to_move is Color.WHITE else '8'
         direction = Square.right if king_side else Square.left
         rook_file = 'h' if king_side else 'a'
@@ -219,6 +223,10 @@ class Castle:
         self.king_move1 = Move(board, to_move, origin, direction(origin))
         self.king_move2 = Move(board, to_move, origin, direction(direction(origin)))
         self.rook_move = Move(board, to_move, Square(f'{rook_file}{rank}'), Square(f'{rook_dest}{rank}'))
+        self.origin = origin
+        self.destination = direction(direction(origin))
+        self.piece_moved: Piece = Piece(PieceType.KING, to_move)
+        self.piece_captured: Piece = None
 
     def execute_move(self):
         self.king_move1.execute_move()
